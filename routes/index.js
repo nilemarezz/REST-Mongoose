9f3d23535b6a1001a0f86601a8d39b1c49843599
Matas <nilenon@gmail.com>
2019-06-30 21:17:49 +0700

    [Add] Edit,Delete

--- routes/index.js
+++ routes/index.js
@@ -14,9 +14,8 @@ router.get('/',async (req,res)=>{
 
 })
 
+// Add Employee
 router.get('/add',(req,res)=> res.render('addEmployee'));
-
-
 router.post('/add',async(req,res)=>{
     let name = req.body.name;
     let age = req.body.age;
@@ -29,5 +28,41 @@ router.post('/add',async(req,res)=>{
 
     res.redirect('/');
 })
+
+router.get('/edit/:id',async(req,res)=>{
+    let employees;
+    try{
+        employees = await employee.findById(req.params.id); 
+    }catch(err){
+        console.log(err);
+    }
+
+    res.render('editEmployee',{employee:employees});
+})
+
+router.put('/edit/:id',async(req,res)=>{
+    let name = req.body.name;
+    let age = req.body.age;
+    let branch = req.body.branch;
+    try{
+        await employee.findOneAndUpdate(req.params.id,{name:name,age:age,branch:branch});
+    }catch(error){
+        console.log(error);
+    }
+
+    res.redirect('/');
+})
+
+router.delete('/delete/:id',async(req,res)=>{
+   
+    try{
+        await employee.findOneAndDelete(req.params.id);
+    }catch(error){
+        console.log(error);
+    }
+
+    res.redirect('/');
+})
+
 
 module.exports = router;
